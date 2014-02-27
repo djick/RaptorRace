0b1a76efe111f9115d4cb4077d7865c3ff74d0f5
Henrik Bøhler <henrik.boehler@gmail.com>
2014-02-27 15:24:41 +0100

    Initial commit

---
+++ RaptorRace/RaptorRace/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RaptorRace
+//
+//  Created by Henrik Bøhler on 27.02.14.
+//  Copyright (c) 2014 iOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RaptorRace/RaptorRaceTests/RaptorRaceTests.m
@@ -0,0 +1,34 @@
+//
+//  RaptorRaceTests.m
+//  RaptorRaceTests
+//
+//  Created by Henrik Bøhler on 27.02.14.
+//  Copyright (c) 2014 iOne. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RaptorRaceTests : XCTestCase
+
+@end
+
+@implementation RaptorRaceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
