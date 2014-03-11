6de14c1bb950b20d740785ce9718e86ae5fb04a2
sirihol <s.holtnaes@gmail.com>
2014-03-11 22:05:53 +0100

    Skeleton

--- RaptorRace/RaptorRace/Obstacles.m
+++ RaptorRace/RaptorRace/Obstacles.m
@@ -14,20 +14,40 @@
 
 -(id)init {
     if (self = [super init]) {
-        _rock= [SKSpriteNode spriteNodeWithImageNamed:@"rock.png"];
-        _rock.size=CGSizeMake(40, 40);
-        _rock.position = CGPointMake(200, 40);
-        _rock.physicsBody=[SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(40,40)];
-        _rock.physicsBody.affectedByGravity=NO;
-        _rock.physicsBody.mass=50000;
-        _rock.physicsBody.allowsRotation=NO;
-        _rock.physicsBody.dynamic=NO; //Making dem obstacles static
-        [self addChild:_rock];
-
+        [self makeRock];
+        [self makeSkeleton];
     }
-    
     return self;
 }
 
 
+-(void)makeRock {
+    _rock= [SKSpriteNode spriteNodeWithImageNamed:@"rock.png"];
+    _rock.size=CGSizeMake(40, 40);
+    _rock.position = CGPointMake(200, 300);
+    _rock.physicsBody=[SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(40,40)];
+    _rock.physicsBody.affectedByGravity=NO;
+    _rock.physicsBody.mass=50000;
+    _rock.physicsBody.allowsRotation=NO;
+    _rock.physicsBody.dynamic=NO; //Making dem obstacles static
+    _rock.xScale = 1;
+    _rock.yScale = 1;
+    [self addChild:_rock];
+
+}
+
+-(void)makeSkeleton {
+    _skeleton= [SKSpriteNode spriteNodeWithImageNamed:@"skeleton1.png"];
+    _skeleton.size=CGSizeMake(40, 40);
+    _skeleton.position = CGPointMake(100, 300);
+    _skeleton.physicsBody=[SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(40,40)];
+    _skeleton.physicsBody.affectedByGravity=NO;
+    _skeleton.physicsBody.mass=50000;
+    _skeleton.physicsBody.allowsRotation=NO;
+    _skeleton.physicsBody.dynamic=NO; //Making dem obstacles static
+    _skeleton.xScale = 2;
+    _skeleton.yScale = 2;
+    [self addChild:_skeleton];
+
+}
 @end
