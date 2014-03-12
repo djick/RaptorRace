caf255b4c0de9798f1110a1d04be2b17e1520c2a
MartinMekk <martin.solheim@me.com>
2014-03-12 13:58:09 +0100

    Created anyRaptor class and added methods in header with comments.

---
+++ RaptorRace/RaptorRace/AnyRaptor.m
@@ -0,0 +1,13 @@
+//
+//  AnyRaptor.m
+//  RaptorRace
+//
+//  Created by Martin Solheim on 12/03/14.
+//  Copyright (c) 2014 iOne. All rights reserved.
+//
+
+#import "AnyRaptor.h"
+
+@implementation AnyRaptor
+
+@end
