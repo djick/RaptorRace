d229a132ac70a88c140ec145f28952cb8e084383
Henriette <henriette.kopstad@gmail.com>
2014-03-13 14:35:59 +0100

    spawning is hard

--- RaptorRace/RaptorRace/GameScene.m
+++ RaptorRace/RaptorRace/GameScene.m
@@ -162,6 +162,9 @@
             [self addChild:sprite];
         }
         
+        self.obs = [[Obstacles alloc]init];
+        [self.obs addObstacles];
+        
     }
     
     return self;
@@ -178,6 +181,8 @@
         self.scoreLabel.text = [NSString stringWithFormat:@"%05.0f", self.score];
         _displayedScore = self.score;
     }
+    //Trying to do da spawning
+    [self.obs spawnObstacle];
 }
 
 
