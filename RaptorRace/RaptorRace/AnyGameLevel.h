62ab5771d491e10153c1f683e93e66ef4e5ce318
MartinMekk <martin.solheim@me.com>
2014-03-16 15:03:37 +0100

    Created AnyGameLevel class This will be the template for creating game levels. AnyGameLevel is a sub-class of SKScene

---
+++ RaptorRace/RaptorRace/AnyGameLevel.h
@@ -0,0 +1,13 @@
+//
+//  AnyGameLevel.h
+//  RaptorRace
+//
+//  Created by Martin Solheim on 16/03/14.
+//  Copyright (c) 2014 iOne. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface AnyGameLevel : SKScene
+
+@end
---
+++ RaptorRace/RaptorRace/AnyGameLevel.m
@@ -0,0 +1,13 @@
+//
+//  AnyGameLevel.m
+//  RaptorRace
+//
+//  Created by Martin Solheim on 16/03/14.
+//  Copyright (c) 2014 iOne. All rights reserved.
+//
+
+#import "AnyGameLevel.h"
+
+@implementation AnyGameLevel
+
+@end
