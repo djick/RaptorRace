ff2af8b509e853e9762d253bd9da87c684e43490
sirihol <s.holtnaes@gmail.com>
2014-03-22 14:26:29 +0100

    one player and two player buttons

--- RaptorRace/RaptorRace/MenuScene.m
+++ RaptorRace/RaptorRace/MenuScene.m
@@ -19,7 +19,8 @@
         sn.size = self.size;
         
         [self addChild:sn];
-        [self addChild: [self newGameButton]];
+        [self addChild: [self onePlayerButton]];
+        [self addChild: [self twoPlayerButton]];
         [self addChild:[self highscoreButton]];
         
         AnyObstacle *obstacle = [[GreenRaptorObstacle alloc] init];
@@ -30,22 +31,38 @@
     return self;
 }
 
-- (SKSpriteNode *)newGameButton
+- (SKSpriteNode *)onePlayerButton
 {
-    SKSpriteNode *newGameButton = [SKSpriteNode spriteNodeWithImageNamed:@"ng"];
-    newGameButton.size = CGSizeMake(175, 38);
-    newGameButton.position = CGPointMake(CGRectGetMidX(self.frame)-100,CGRectGetMidY(self.frame)+20);
-    newGameButton.name = @"newGameButton";
-    return newGameButton;
+    SKSpriteNode *onePlayerButton = [SKSpriteNode spriteNodeWithImageNamed:@"1p"];
+    onePlayerButton.size = CGSizeMake(175, 38);
+    onePlayerButton.position = CGPointMake(CGRectGetMidX(self.frame)-70,CGRectGetMidY(self.frame)+30);
+    onePlayerButton.name = @"onePlayerButton";
+    onePlayerButton.xScale*=0.8;
+    onePlayerButton.yScale*=0.8;
+    return onePlayerButton;
+    
+}
+
+- (SKSpriteNode *)twoPlayerButton
+{
+    SKSpriteNode *twoPlayerButton = [SKSpriteNode spriteNodeWithImageNamed:@"2p"];
+    twoPlayerButton.size = CGSizeMake(175, 38);
+    twoPlayerButton.position = CGPointMake(CGRectGetMidX(self.frame)+70,CGRectGetMidY(self.frame)+30);
+    twoPlayerButton.name = @"twoPlayerButton";
+    twoPlayerButton.xScale*=0.8;
+    twoPlayerButton.yScale*=0.8;
+    return twoPlayerButton;
     
 }
 
 - (SKSpriteNode *)highscoreButton
 {
-    SKSpriteNode *highscoreButton = [SKSpriteNode spriteNodeWithImageNamed:@"hs"];
+    SKSpriteNode *highscoreButton = [SKSpriteNode spriteNodeWithImageNamed:@"hs1"];
     highscoreButton.size = CGSizeMake(175, 38);
-    highscoreButton.position = CGPointMake(CGRectGetMidX(self.frame)+100,CGRectGetMidY(self.frame)+20);
+    highscoreButton.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame)-10);
     highscoreButton.name = @"highscoreButton";
+    highscoreButton.xScale*=0.8;
+    highscoreButton.yScale*=0.8;
     return highscoreButton;
     
 }
@@ -57,8 +74,13 @@
     CGPoint location = [touch locationInNode:self];
     SKNode *node = [self nodeAtPoint:location];
 
-    if ([node.name isEqualToString:@"newGameButton"]) {
-        NSLog(@"New game button pressed");
+    if ([node.name isEqualToString:@"onePlayerButton"]) {
+        NSLog(@"One player button pressed");
+        MainGameScene *game = [[MainGameScene alloc] initWithSize:self.size];
+        [self.view presentScene:game transition:[SKTransition doorsOpenVerticalWithDuration:0.5]];
+    }
+    if ([node.name isEqualToString:@"twoPlayerButton"]) {
+        NSLog(@"Two player button pressed");
         MainGameScene *game = [[MainGameScene alloc] initWithSize:self.size];
         [self.view presentScene:game transition:[SKTransition doorsOpenVerticalWithDuration:0.5]];
     }
